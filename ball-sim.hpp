5dcc5b301c3ef891190cbe6a55aeb93b9b49e843
kasem nassar <nassarkn94@gmail>
2025-04-03 23:12:40 -0400

    updated implementation to add more balls in a single frame

--- ball-sim.hpp
+++ ball-sim.hpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <random>
 #include "state.hpp"
 
 constexpr int8_t px_per_meter = 100;
 constexpr float g = 9.81f * px_per_meter; // 981 px per s^2
 constexpr float dt = 0.016f;              // Time step (s) for 60FPS
 constexpr float e = 1.0f;                 // elasticity [0, 1] where 0(no bounce) and 1(perfect bounce)
+constexpr float ball_radius = 20.0f;
 
 class ball
 {
 public:
-    ball(const position2d &pos, const velocity2d &vel)
-        : state_(pos, vel) {}
+    ball(const velocity2d &vel = velocity2d{0.0f, 0.0f}, float radius = ball_radius)
+        : state_({getRandomX(), 0.0f}, vel), radius_{radius}
+    {
+        shape_.setRadius(radius_);
+        shape_.setFillColor(sf::Color::Red);
+        shape_.setOrigin(radius_, radius_);
+        shape_.setPosition(state_.position.x, state_.position.y);
+    }
 
+    void draw(sf::RenderWindow &window) const;
     void update();
     state getState() const;
 
@@ -19,6 +29,9 @@ private:
     void updatePosition2d();
     void updateVelocity2d();
     float bounce2d();
+    static float getRandomX();
 
     state state_;
+    const float radius_;
+    sf::CircleShape shape_;
 };
--- main.cc
+++ main.cc
@@ -6,13 +6,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Bouncing Ball Sim");
     window.setFramerateLimit(60);
 
-    position2d pos(400.0f, 100.0f); // 400px from left, 100px from top (in px)
-    velocity2d vel(0.0f, 0.0f);
-    ball b(pos, vel);
+    std::vector<ball> particles;
 
-    sf::CircleShape shape(20.0f); // radius in pixels
-    shape.setFillColor(sf::Color::Red);
-    shape.setOrigin(20.0f, 20.0f); // center origin
+    for (int i = 0; i < 10; i++)
+    {
+        particles.push_back(ball{});
+    }
 
     while (window.isOpen())
     {
@@ -23,15 +22,14 @@ int main()
                 window.close();
         }
 
-        b.update();
-
         window.clear(sf::Color::White);
 
-        // Get ball's updated position in pixels
-        const state s = b.getState();
-        shape.setPosition(s.position.x, s.position.y);
+        for (int j = 0; j < 10; j++)
+        {
+            particles[j].update();
+            particles[j].draw(window);
+        }
 
-        window.draw(shape);
         window.display();
     }
 
