67319d9ff10d0d358ac7387259c393bf14d42eee
kasem nassar <nassarkn94@gmail>
2025-04-03 23:39:50 -0400

    removed radius from constructor

--- ball-sim.hpp
+++ ball-sim.hpp
@@ -6,14 +6,13 @@
 constexpr int8_t px_per_meter = 100;
 constexpr float g = 9.81f * px_per_meter; // 981 px per s^2
 constexpr float dt = 0.016f;              // Time step (s) for 60FPS
-constexpr float e = 1.0f;                 // elasticity [0, 1] where 0(no bounce) and 1(perfect bounce)
-constexpr float ball_radius = 20.0f;
+constexpr float e = 0.8f;                 // elasticity [0, 1] where 0(no bounce) and 1(perfect bounce)
 
 class ball
 {
 public:
-    ball(const velocity2d &vel = velocity2d{0.0f, 0.0f}, float radius = ball_radius)
-        : state_({getRandomX(), 0.0f}, vel), radius_{radius}
+    ball(const velocity2d &vel = velocity2d{0.0f, 0.0f})
+        : state_({getRandomX(), 0.0f}, vel)
     {
         shape_.setRadius(radius_);
         shape_.setFillColor(sf::Color::Red);
@@ -21,8 +20,8 @@ public:
         shape_.setPosition(state_.position.x, state_.position.y);
     }
 
-    void draw(sf::RenderWindow &window) const;
     void update();
+    void draw(sf::RenderWindow &window) const;
 
 private:
     void updatePosition2d();
@@ -31,6 +30,6 @@ private:
     static float getRandomX();
 
     state state_;
-    const float radius_;
+    const float radius_{20.0f};
     sf::CircleShape shape_;
 };
